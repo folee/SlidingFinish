96ef6580a44c7c88b340408096dfb037fb608a3a
Folee.W <folee.w@gmail.com>
2014-04-09 14:03:59 +0800

    add project

---
+++ src/com/example/slidingfinish/ScrollActivity.java
@@ -0,0 +1,39 @@
+package com.example.slidingfinish;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.view.SildingFinishLayout;
+import com.example.view.SildingFinishLayout.OnSildingFinishListener;
+
+public class ScrollActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scroll);
+
+
+		SildingFinishLayout mSildingFinishLayout = (SildingFinishLayout) findViewById(R.id.sildingFinishLayout);
+		mSildingFinishLayout
+				.setOnSildingFinishListener(new OnSildingFinishListener() {
+
+					@Override
+					public void onSildingFinish() {
+						ScrollActivity.this.finish();
+					}
+				});
+
+	}
+	
+	
+	// Press the back button in mobile phone
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		overridePendingTransition(0, R.anim.base_slide_right_out);
+	}
+
+}
